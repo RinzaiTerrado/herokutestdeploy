3f45c43c246cbd16ac9d8dea47e3d19e38fe9bf7
RinzaiTerrado <75122826+RinzaiTerrado@users.noreply.github.com>
2022-07-04 19:30:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from PIL import Image
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
-
+p = int(os.environ.get("PORT", 5000))
 
 @app.route("/", methods=['POST',"GET"])
 def upload_image():
     return render_template("index.html")
+app.run(debug=True, port=p, host='0.0.0.0')
 
