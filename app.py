712e34e77e8a4d29108eb2027ba8635ed2c091d2
RinzaiTerrado <75122826+RinzaiTerrado@users.noreply.github.com>
2022-07-04 19:14:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 from flask import Flask, render_template,request
 import os
-import numpy as np
-import mediapipe as mp
-import matplotlib.pyplot as plt
-from PIL import Image
-from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 
