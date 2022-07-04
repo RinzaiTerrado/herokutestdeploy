a94c1164b7cf11c927c27fa81bffa494f385230d
RinzaiTerrado <75122826+RinzaiTerrado@users.noreply.github.com>
2022-07-04 19:05:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask import Flask, render_template,request
 app = Flask(__name__)
 
 
-port = int(os.environ.get('PORT', 5000))
 
 @app.route("/", methods=['POST',"GET"])
 def upload_image():
     return render_template("index.html")
 
-app.run(host='0.0.0.0', port=port, debug=True)
