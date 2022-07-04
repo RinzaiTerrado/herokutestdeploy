c940bf3993ea9106cd21414084e39cc0b15c30ca
RinzaiTerrado <75122826+RinzaiTerrado@users.noreply.github.com>
2022-07-04 21:24:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template,request
 import os
 import numpy as np
 from PIL import Image
-import cv2
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
